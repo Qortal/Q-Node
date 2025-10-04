ed40b58fdfd5c840206690a9b4ac73556eff1968
Nic <gitnub.6q580@simplelogin.com>
2025-10-04 17:01:42 +0200

    Refactor primary response (not an array)

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,10 @@ import {
   useState,
 } from 'react';
 import { useTranslation } from 'react-i18next';
-import { TIME_MINUTES_1_IN_MILLISECONDS, TIME_MINUTES_2_IN_MILLISECONDS, TIME_MINUTES_5_IN_MILLISECONDS } from './common/constants';
+import {
+  TIME_MINUTES_1_IN_MILLISECONDS,
+  TIME_MINUTES_2_IN_MILLISECONDS,
+} from './common/constants';
 
 function secondsToDhms(seconds: number) {
   seconds = Number(seconds);
@@ -524,21 +527,24 @@ function App() {
 
     fetchNodeData(); // fetch once immediately
 
-    const intervalId = setInterval(fetchNodeData, TIME_MINUTES_1_IN_MILLISECONDS);
+    const intervalId = setInterval(
+      fetchNodeData,
+      TIME_MINUTES_1_IN_MILLISECONDS
+    );
 
     return () => clearInterval(intervalId);
   }, []);
 
   async function getNameInfo(address: string) {
-    const nameLink = `/names/address/${address}?limit=0&reverse=true`;
-    const nameResponse = await fetch(nameLink);
+    const primary = `/names/primary/${address}`;
+    const nameResponse = await fetch(primary);
     const nameResult = await nameResponse.json();
-    if (nameResult?.length > 0) {
+    if (nameResult != null) {
       return {
-        name: nameResult[0].name,
+        name: nameResult.name,
         avatar:
           '/arbitrary/THUMBNAIL/' +
-          nameResult[0].name +
+          nameResult.name +
           '/qortal_avatar?async=true',
       };
     } else {
@@ -551,13 +557,12 @@ function App() {
 
   async function getMintingAccounts() {
     setLoadingMintingAccountsTable(true);
-
     try {
       const list = await qortalRequest({
         action: 'ADMIN_ACTION',
         type: 'getmintingaccounts',
       });
-      
+
       // Enrich in parallel and WAIT for them
       const enriched = await Promise.all(
         list.map(async (item) => {
@@ -571,7 +576,6 @@ function App() {
           };
         })
       );
-
       setMintingAccounts(enriched);
     } catch (err) {
       console.error(err);
